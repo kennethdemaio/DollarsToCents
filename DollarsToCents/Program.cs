3b32924337ee3df79bde790464a62d0142b41e5e
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-09 13:52:00 -0400

    Basic formatting, logic not finished

--- DollarsToCents/Program.cs
+++ DollarsToCents/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Github integration test");
+﻿Console.WriteLine("Enter a sum to convert from dollars to pennies");
+string response = Console.ReadLine();
+int ResponseInt = Convert.ToInt32(response);
+int ConvertedInt = (100 * ResponseInt);
+Console.WriteLine("If we convert " +ResponseInt + "$ to pennies we will have " + ConvertedInt + "¢ cents");
+Console.ReadKey();
